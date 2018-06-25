7ed4b0700e7e946659006afebce43f3a8a1c3887
ahhbee0127 <ahhbee_0127@hotmail.com>
2018-06-25 10:35:43 +0800

    0001 - Added firebase into project

---
+++ NutritionClub/app/src/main/java/com/example/nutritionclub/AccountActivity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.nutritionclub.AccountActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nutritionclub.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ NutritionClub/app/src/main/java/com/example/nutritionclub/AccountActivity/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.example.nutritionclub.AccountActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nutritionclub.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
